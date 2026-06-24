2c7512bdc82be8b8cd128fe676b12dcddebef24f
Gurleen Kaur <gurleenkaur97520@gmail.com>
2026-06-23 22:31:22 -0600

    Update HumanPlayer.cs

--- ConnectFour/ConnectFour/HumanPlayer.cs
+++ ConnectFour/ConnectFour/HumanPlayer.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ConnectFour
+﻿namespace ConnectFour
 {
-    internal class HumanPlayer
+    public class HumanPlayer : Player
     {
+        public HumanPlayer(string name, char symbol) : base(name, symbol) { }
+
+        public override int ChooseColumn(Board board)
+        {
+            return ConsoleView.GetColumnInput(Name);
+        }
     }
 }
