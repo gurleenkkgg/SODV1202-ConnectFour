56e379699216e28de5e4bf9106ec86429bcf2176
navdeepsidhuuu <navdeep80singh88@gmail.com>
2026-06-24 11:31:36 -0600

    Update GameController.cs

--- ConnectFour/ConnectFour/GameController.cs
+++ ConnectFour/ConnectFour/GameController.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ConnectFour
+if (board.PlacePiece(chosenCol, currentPlayer.Symbol))
 {
-    internal class GameController
+    if (board.CheckHorizontalWin(currentPlayer.Symbol) || 
+        board.CheckVerticalWin(currentPlayer.Symbol) || 
+        board.CheckDiagonalWin(currentPlayer.Symbol))
     {
+        ConsoleView.DisplayBoard(board);
+        Console.WriteLine($"🎉 CONGRATULATIONS! {currentPlayer.Name} ({currentPlayer.Symbol}) wins!");
+        running = false;
+    }
+    else if (board.IsBoardFull())
+    {
+        ConsoleView.DisplayBoard(board);
+        Console.WriteLine("🤝 It's a draw! The board is full.");
+        running = false;
+    }
+    else    {
+        currentPlayer = (currentPlayer == player1) ? player2 : player1;
     }
 }
