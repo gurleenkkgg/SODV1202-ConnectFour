d0b8655d7f9c58ac14fc39702092df7e667629f6
Gurleen Kaur <gurleenkaur97520@gmail.com>
2026-06-25 08:32:12 -0600

    Add horizontal and vertical win checks to Board class

--- ConnectFour/ConnectFour/Board.cs
+++ ConnectFour/ConnectFour/Board.cs
@@ -27,5 +27,32 @@ namespace ConnectFour{
             }
             return false;
         }
+    } 
+    public bool CheckHorizontalWin(char symbol)
+{
+    for (int r = 0; r < 6; r++)
+    {
+        for (int c = 0; c < 4; c++)
+        {
+            if (grid[r, c] == symbol && grid[r, c+1] == symbol && 
+                grid[r, c+2] == symbol && grid[r, c+3] == symbol)
+                return true;
+        }
     }
+    return false;
+}
+
+public bool CheckVerticalWin(char symbol)
+{
+    for (int c = 0; c < 7; c++)
+    {
+        for (int r = 0; r < 3; r++)
+        {
+            if (grid[r, c] == symbol && grid[r+1, c] == symbol && 
+                grid[r+2, c] == symbol && grid[r+3, c] == symbol)
+                return true;
+        }
+    }
+    return false;
+}
 }
